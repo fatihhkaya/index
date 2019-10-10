521c2b92d0ee9e9fbdeb2f5c5d795c5e27f692bf
Fatih Kaya <fatih.55kaya.1@gmail.com>
2019-10-11 00:29:28 +0300

    Updates

---
+++ precache-manifest.ce815353078af691d74a58a55ab623a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c997134f1e7e01fc8d25e126e4836a4b",
+    "url": "/index/index.html"
+  },
+  {
+    "revision": "ef09f100be57b3db1991",
+    "url": "/index/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "41c3931820b9edf5526a",
+    "url": "/index/static/js/2.23234df5.chunk.js"
+  },
+  {
+    "revision": "ef09f100be57b3db1991",
+    "url": "/index/static/js/main.b9559f4c.chunk.js"
+  },
+  {
+    "revision": "a61303e721383f6ff1fc",
+    "url": "/index/static/js/runtime-main.9e43d9bf.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/index/static/media/logo.25bf045c.svg"
+  }
+]);
